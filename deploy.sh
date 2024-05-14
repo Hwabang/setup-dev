3d0317e44091d833256317c3f2f63d3509b72355
jeonghi <jpark0902@kookmin.ac.kr>
2024-05-15 00:48:07 +0900

    [Remove] 불필요한 도커파일 제거/ [Modify] 배포 스크립트 수정

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 
-# Pull latest container versions
-docker-compose pull
+# 현재 실행 중인 모든 Docker 컨테이너를 종료하고 제거
+echo "Stopping all running containers..."
+docker stop $(docker ps -aq)
 
-# Build containers
-docker-compose build
+echo "Removing all containers..."
+docker rm $(docker ps -aq)
 
-# Launch the containers
+# 모든 Docker 이미지 제거 (선택적: 주의해서 사용하세요)
+# echo "Removing all docker images..."
+# docker rmi $(docker images -q)
+
+# 모든 Docker 네트워크 제거
+echo "Removing all docker networks..."
+docker network prune -f
+
+# Docker 볼륨 정리 (사용하지 않는 볼륨 삭제)
+echo "Pruning volumes..."
+docker volume prune -f
+
+# Docker 시스템 정리 (불필요한 데이터 모두 삭제)
+echo "Pruning Docker system..."
+docker system prune -af
+
+# 새로운 Docker Compose 프로젝트 실행
+echo "Starting new Docker Compose project..."
 docker-compose up -d
 
-# First run Certbot to get SSL certificates
-docker-compose run --rm certbot certonly --webroot --webroot-path=/var/www/certbot -d hwabang.jeonghi.com --agree-tos --email jpakr0902@kookmin.ac.kr --no-eff-email
-
-# Restart Nginx to use the new certificates
-docker-compose restart nginx
+echo "Deployment completed successfully."
